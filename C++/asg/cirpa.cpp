f51ab19a840c59e50667f73e262f8cab74097570
anil-650 <anilhansda65@gmail.com>
2021-08-14 13:04:47 +0530

    update'

--- C++/asg/cirpa.cpp
+++ C++/asg/cirpa.cpp
@@ -8,21 +8,30 @@ using namespace std;
 #define PI 3.14
 
 class circle {
-    int x, y;
+    float r, a, p;
     public:
-    void getno() {
-        cout << "Enter 2 numbers\n";
-        cin >> x >> y;
+    void rad() {
+        cout << "Enter radius od the circle\n";
+        cin >> r;
+        cout << "\nRadius of the circle : " << r << endl;
     }
-    void suv() {
-        cout << x << "-" << y << "=" << x - y;
+
+    void area() {
+        a = PI*r*r;
+        cout << "Area of the circle : " << a << endl ;
+    }
+
+    void peri(){
+        p = 2*PI*r;
+        cout << "Perimeter of the circle : " << p << endl ;
     }
 };
 int main()
 {
-    subtract no;
-    no.getno();
-    no.suv();
+    circle o;
+    o.rad();
+    o.area();
+    o.peri();
 
     return 0;
 }
